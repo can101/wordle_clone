f8d2a971f0f7b896fe009d0ab3746e989191d049
Can Çelik <68447137+can101@users.noreply.github.com>
2022-03-14 07:10:19 +0300

    Update index.js
    new code line added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const axios = require("axios").default;
+const CryptoJS = require("crypto-js");
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
 let randomWords = require("random-words");
-// let _ = require("lodash");
 
 const app = express();
 app.use(express.static("./src"));
@@ -15,8 +15,18 @@ app.get("/", (req, res) => {
 });
 
 app.get("/word", (req, res) => {
-  let words = randomWords({ exactly: 10, wordsPerString: 1, min: 5, maxLength: 5, formatter: (word) => word.toUpperCase() }).filter((word) => word.length === 5);
-  res.json(words);
+//   let words = randomWords({ exactly: 10, wordsPerString: 1, min: 5, maxLength: 5, formatter: (word) => word.toUpperCase() }).filter((word) => word.length === 5);
+//   res.json(words);
+ let words = randomWords({
+    exactly: 10,
+    wordsPerString: 1,
+    min: 5,
+    maxLength: 5,
+    formatter: (word) => word.toUpperCase(),
+  }).filter((word) => word.length === 5);
+  const myPassword = process.env.SALT_KEY;
+  let encrypted = CryptoJS.AES.encrypt(JSON.stringify(words), myPassword).toString();
+  res.json({ dados: encrypted });
 });
 
 const PORT = process.env.PORT || 3500;
